94c706d83f6c742a14e42d84c238e4add8710a92
hsiehjohnny <106021383@live.asia.edu.tw>
2017-09-25 16:28:39 +0800

    786

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float a = 2;
+        System.out.println(Math.round(a * 0.26418f * 10f) / 10f);
+        float b = 1.5f;
+        System.out.println(Math.round(b * 1000 / 600 * 10f) / 10f);
+        Scanner scn = new Scanner(System.in);
+
+
+
+
+    }
+}
